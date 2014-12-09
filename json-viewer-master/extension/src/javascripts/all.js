d081f00cfb6ad9e97358e91ad57c22420350ee6e
Ranadeep Polavarapu <ranadeep@icloud.com>
2014-12-09 13:42:35 -0500

    Initial commit. Fork from jsonv.

---
+++ json-viewer-master/extension/src/javascripts/all.js
@@ -0,0 +1,8 @@
+//= require prism.js
+//= require prism-code-folder.js
+//= require jsl-format.js
+//= require content_util.js
+//= require content.js
+//= require_self
+
+document.addEventListener("DOMContentLoaded", contentReady, false);
